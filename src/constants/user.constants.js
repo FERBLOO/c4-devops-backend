6aecfcef40930f92d539e1c473755cbeb67bb9bd
ferbloo <jfernandocortes@hotmail.com>
2021-11-29 21:12:19 -0500

    cargue a gitHub

---
+++ src/constants/user.constants.js
@@ -0,0 +1,16 @@
+const ROLES = {
+    ADMIN,
+    LEADER,
+    STUDENT,
+};
+
+const USER_STATUS = {
+    PENDING,
+    AUTHORIZED,
+    UNAUTHORIZED,
+};
+
+export {
+    ROLES, 
+    USER_STATUS,
+};
