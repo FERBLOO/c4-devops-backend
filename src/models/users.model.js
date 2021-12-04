f59293d60a5573df92603fcdb46002e2cc0c9c6b
ferbloo <jfernandocortes@hotmail.com>
2021-12-03 21:58:37 -0500

    Corrección errores en usuarios

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,34 +5,41 @@ const usersSchema = new Schema({
   email: {
     type: String,
     unique: true,
+    required: true,
   },
   documentId: {
     type: Number,
     unique: true,
+    required: true,
   },
   name: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
+    required: true,
   },
   fullName: {
     type: String,
+    required: true,
   },
   role: {
     type: String,
-    enum: ['ADMIN', 'LEADER', 'STUDENT']
+    enum: ['ADMIN', 'LEADER', 'STUDENT'],
+    required: true,
   },
   status: {
     type: String,
-    enum: ['PENDING', 'AUTHORIZED', 'UNAUTHORIZED']
+    enum: ['PENDING', 'AUTHORIZED', 'UNAUTHORIZED'],
+    required: true,
   },
   password: {
     type: String,
+    required: true,
   }
 }, { versionKey: false });
 
 const Users = new mongoose.model('users', usersSchema);
 
 export default Users;
-
